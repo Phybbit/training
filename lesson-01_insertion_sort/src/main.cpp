f49a90a436ed6e01c1ab01b25f58aedddf7add8e
Eurico Doirado <eurico@phybbit.com>
2015-06-14 18:24:10 +0900

    Added comments for lesson 01.

--- lesson-01_insertion_sort/src/main.cpp
+++ lesson-01_insertion_sort/src/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 
+#define ELEMENTS_COUNT 10
+
 int* sort(int unsorted[]) {
-  // implement
+  // Implement the insertion sort algorithm.
 
   return unsorted;
 }
 
 
 int main() {
-  const int elementsCount = 10;
-  int unsorted[elementsCount] = { 2, 10, 17, 8, 13, 7, 9, 12, 11, 1 };
+  // Initial data: hardcoded array of random values.
+  int unsorted[] = { 2, 10, 17, 8, 13, 7, 9, 12, 11, 1 };
 
+  // Print out our initial array
   std::cout << "Input: ";
-  for (int i = 0; i < elementsCount; i++) {
+  for (int i = 0; i < ELEMENTS_COUNT; i++) {
     std::cout << unsorted[i] << " ";
   }
   std::cout << std::endl << std::endl;
 
-
+  // Sort
   int* sortedResult = sort(unsorted);
 
+  // Print out our sorted array
   std::cout << "Output: ";
-  for (int i = 0; i < elementsCount; i++) {
+  for (int i = 0; i < ELEMENTS_COUNT; i++) {
     std::cout << sortedResult[i] << " ";
   }
   std::cout << std::endl << std::endl;
